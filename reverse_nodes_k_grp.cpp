7759ce4719162d21345200b2db036a5b3aea0135
Soham Chakraborty <81106480+Soham20chak@users.noreply.github.com>
2022-11-08 00:24:33 +0530

    Create reverse_nodes_k_grp.cpp

---
+++ reverse_nodes_k_grp.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
+        int c=0;
+        int i=0;
+        ListNode* x=head;
+        while(x!=NULL && i<k){
+            x=x->next;
+            i++;
+        }
+        if(i<k){
+            return head;
+        }
+        
+        
+        while(curr!= NULL && c<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            c++;
+        }
+        if(next!=NULL){
+           head->next=reverseKGroup(next,k); 
+        }
+        return prev;
+    }
+};
