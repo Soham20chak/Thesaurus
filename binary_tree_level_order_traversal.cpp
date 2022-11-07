c6c5bc0edc212e3979ff213d322902f5c2bd3f28
Soham Chakraborty <81106480+Soham20chak@users.noreply.github.com>
2022-11-08 00:29:46 +0530

    Create binary_tree_level_order_traversal.cpp

---
+++ binary_tree_level_order_traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        if(root==NULL) return ans;
+        q.push(root);
+        while(!(q.empty())){
+            int size=q.size();
+            //for each level
+            vector<int>level;
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left !=NULL) q.push(node->left);
+                if(node->right !=NULL) q.push(node->right);
+                level.push_back(node->val);
+                
+            }
+            ans.push_back(level);
+            
+        }
+        return ans;
+    }
+};
