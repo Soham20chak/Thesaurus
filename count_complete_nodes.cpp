6c474eca13c50c930613c32a7e2821ed8fbfec0f
Soham Chakraborty <81106480+Soham20chak@users.noreply.github.com>
2022-11-07 23:28:44 +0530

    Create count_complete_nodes.cpp

---
+++ count_complete_nodes.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+      if(root==NULL)
+            return 0;
+        return countNodes(root->left)+countNodes(root->right)+1;
+    }
+};
